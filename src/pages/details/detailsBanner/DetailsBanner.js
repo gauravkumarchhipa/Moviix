15b5bd24630dbcd55e78d9981f22e25b6286198f
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-04-09 11:02:33 +0530

    third commit

---
+++ src/pages/details/detailsBanner/DetailsBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailsBanner = () => {
+  return (
+    <div>DetailsBanner</div>
+  )
+}
+
+export default DetailsBanner
---
+++ src/pages/home/trending/Trending.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+const Trending = () => {
+  return (
+    <div className='carouselSection'>
+      <ContentWrapper>
+        <spam className="carouselTitle">Trending</spam>
+      </ContentWrapper>
+
+    </div>
+  )
+}
+
+export default Trending 
